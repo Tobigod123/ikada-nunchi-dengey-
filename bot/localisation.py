80b943479f07b1c1976364aac351e9d6cbf30304
Gns <97271133+Gns09@users.noreply.github.com>
2023-09-24 09:35:23 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -23,7 +23,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [AnimeSpectrum.mkv]"
+    COMPRESS_SUCCESS = "{kk}"
 
     COMPRESS_PROGRESS = "🌄 ETA: {} 💦 Progress: {}%"
 
